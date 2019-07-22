8c787f5741f29b2102b8b6bc1d6112c83d39fa1c
Mohamad Bwidani <mbwedany@gmail.com>
2019-07-22 11:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 function scuberGreetingForFeet(feetValue){
   // Write your code here!
   let charge;
-    if (feetValue <= 400){
-      charge = "This one is on me!";
-    }
-    else if (feetValue > 2000 && feetValue <= 2500){
-<<<<<<< HEAD
-      charge = "I will gladly take your thirty bucks.";
-    }
-    else {
-      charge = "No can do.";
-=======
-      charge = "I will gladly take your thirty bucks";
-    }
-    else {
-      charge = "Oops, Sorry";
->>>>>>> ea12ec25fad6e9233f85684ba960e09abc40dd8b
-    }
-    return charge;
+  if (feetValue <= 400){
+    charge = "This one is on me!";
+  }
+  else if (feetValue > 2000 && feetValue <= 2500){
+    charge = "I will gladly take your thirty bucks.";
+  }
+  else {
+    charge = "No can do.";
+  }
+  return charge;
 }
 
 function ternaryCheckCity(city){
