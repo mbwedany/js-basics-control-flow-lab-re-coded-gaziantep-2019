e39cc6df6e20c52829de752844bf06f4bff3dd8d
Mohamad Bwidani <mbwedany@gmail.com>
2019-07-22 11:44:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetValue){
   // Write your code here!
+  let charge;
+    if (feetValue <= 400){
+      charge = "This one is on me!";
+    }
+    else if (feetValue > 2000 && feetValue <= 2500){
+      charge = "I will gladly take your thirty bucks.";
+    }
+    else {
+      charge = "No can do.";
+    }
+    return charge;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let forCity;
+  if (city === "NYC"){
+    forCity = "Ok, sounds good.";
+  }
+  else{
+    forCity = "No go.";
+  }
+  return forCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tips){
   // Write your code here!
+  let tipAnswer;
+  
+  
+  if (Tips === "generous"){
+    tipAnswer = "Thank you so much.";
+  }
+  else if (Tips === "not as generous"){
+    tipAnswer = "Thank you.";
+  }
+  else {
+    tipAnswer = "Bye.";
+  }
+  return tipAnswer;
 }
